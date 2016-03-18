c517eb5e6d05191d2cae90151dd7743d7f658e59
Thiago Almeida <tbdealmeida@gmail.com>
2016-03-18 12:15:09 +0000

    commit

--- src/main/java/com/walmart/logistics/api/AppTest.java
+++
@@ -1,8 +0,0 @@
-package com.walmart.logistics.api;
-
-public class AppTest {
-
-	public static void main(String[] args) {
-		System.out.println();
-	}
-}
---
+++ src/main/java/com/walmart/logistics/api/SpringBootLabApplication.java
@@ -0,0 +1,12 @@
+package com.walmart.logistics.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLabApplication.class, args);
+    }
+}
