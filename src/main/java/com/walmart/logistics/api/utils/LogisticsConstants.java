e6777177e4d512d810f86883c3ff782047935610
Thiago Almeida <tbdealmeida@gmail.com>
2016-03-19 14:14:10 +0000

    Constants

---
+++ src/main/java/com/walmart/logistics/api/utils/LogisticsConstants.java
@@ -0,0 +1,7 @@
+package com.walmart.logistics.api.utils;
+
+public final class LogisticsConstants {
+
+	public static final String MESSAGE_MAP_ERROR = "ERROR: Map already registered";
+	public static final String MESSAGE_MAP_SUCCESS = "OK: Map created successfully";
+}
